59de6e12c01e54896c80565b3f6a98a96f2aeeca
Zohaib Sheraz Mughal <zohaibsheraz80@gmail.com>
2024-12-11 23:24:02 +0100

    TESTING NOW, added pip command in tests sh file

--- project/tests.sh
+++ project/tests.sh
@@ -3,5 +3,5 @@
 
 # please provide your kaggle username and kaggle key in pipeline.py file in "if" block at the bottom
 # in Pipeline and Test constructors as the placeholders specified
-
+pip install -r requirements.txt
 python ./project/pipeline.py
